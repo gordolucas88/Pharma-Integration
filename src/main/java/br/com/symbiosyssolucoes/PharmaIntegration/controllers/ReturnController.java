536ea90fe46449d4b4268a1569adad0d91995018
Lucas Rodrigues <lucasrodrigues062@gmail.com>
2021-03-18 16:15:16 -0300

    Mudado o tipo de Verbo da rota de retorno

--- src/main/java/br/com/symbiosyssolucoes/PharmaIntegration/controllers/ReturnController.java
+++ src/main/java/br/com/symbiosyssolucoes/PharmaIntegration/controllers/ReturnController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -18,7 +19,7 @@ public class ReturnController {
     ReturnService returnService;
 
 
-    @PostMapping("/return")
+    @PutMapping("/return")
     public ResponseEntity<PedidoPalm> retornoEMS(@RequestBody PedidoPalm pedidoPalm){
 
 
